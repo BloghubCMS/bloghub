eef6451b4814a673ca61fe9f5f251314c0b330ce
Christopher Dobler <chrisdobler@gmail.com>
2017-07-08 12:28:49 -0700

    Adds Welcome ASCII

--- api/index.js
+++ api/index.js
@@ -30,5 +30,14 @@ server.listen(3030, () => {
   const host = server.address().address;
   const port = server.address().port;
 
-  console.log('Api is listening on http://%s:%s', host, port);
+  console.log(`
+  ____  _             _   _       _     
+ | __ )| | ___   __ _| | | |_   _| |__  
+ |  _ \\| |/ _ \\ / _\` | |_| | | | | '_ \\ 
+ | |_) | | (_) | (_| |  _  | |_| | |_) |
+ |____/|_|\\___/ \\__, |_| |_|\\__,_|_.__/ 
+                |___/ 
+
+`);
+  console.log('Bloghub api is listening on http://%s:%s', host, port);
 });
